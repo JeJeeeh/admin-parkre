a5d44020c10dfa49f226e385e6e88ed38ed1f7a2
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2022-11-23 03:00:30 +0700

    Merge of "main" and "origin/main"

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,9 @@ Route::get('/', [SiteController::class, 'index'])->name('index');
 Route::get('login', [SiteController::class, 'login'])->name('login');
 Route::post('login', [SiteController::class, 'doLogin'])->name('doLogin');
 Route::get('register', [SiteController::class, 'register'])->name('register');
-<<<<<<< Updated upstream
+
 Route::post('register', [SiteController::class, 'doRegister'])->name('doRegister');
-=======
 
 Route::prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('admin.home');
 });
->>>>>>> Stashed changes
