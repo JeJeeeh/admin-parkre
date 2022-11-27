d556b49b5f084a841109a83e1a1d958218c20ab5
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2022-11-27 21:20:27 +0700

    Add mall controller and routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\MallController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('user', function () {
-    return response()->json([
-        'message' => 'success',
-    ], 200);
+Route::prefix('mall')->group(function () {
+    Route::get('/', [MallController::class, 'index'])->name('mall.get');
+    Route::post('/', [MallController::class, 'store'])->name('mall.post');
+    Route::put('/{id}', [MallController::class, 'update'])->name('mall.put');
+    Route::delete('/{id}', [MallController::class, 'destroy'])->name('mall.delete');
+
+    Route::get('/{id}', [MallController::class, 'show'])->name('mall.find');
+});
+
+Route::prefix('user')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('user.get');
+    Route::post('/', [UserController::class, 'store'])->name('user.post');
+    Route::put('/{id}', [UserController::class, 'update'])->name('user.put');
+    Route::delete('/{id}', [UserController::class, 'destroy'])->name('user.delete');
+
+    Route::get('/{id}', [UserController::class, 'show'])->name('user.find');
 });
