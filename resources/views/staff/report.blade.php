dd865d04855e6ac10fb54aa8377f486798d83014
AwuAwuu <nicholas_s20@mhs.istts.ac.id>
2022-11-27 21:14:35 +0700

    hiya

--- resources/views/staff/report.blade.php
+++ resources/views/staff/report.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="mt-16 py-8 px-4">
         <div>
-            <p class="text-semibold text-3xl">Announcement List</p>
+            <p class="text-semibold text-3xl">Report List</p>
         </div>
     </div>
 @endsection
