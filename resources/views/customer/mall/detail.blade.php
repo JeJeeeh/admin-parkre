7bfc5c07a671fffe230ef7c5f2a6bfec9d1e84b8
Leozer HD <leonardminoto@gmail.com>
2022-12-22 19:51:39 +0700

    foto mall di halaman customer

--- resources/views/customer/mall/detail.blade.php
+++ resources/views/customer/mall/detail.blade.php
@@ -8,10 +8,27 @@
 
 <div class="flex flex-col space-y-12 my-24 mx-12">
     <div class="flex space-x-8">
+<<<<<<< Updated upstream
         <div class="rounded-md">
             <figure>
                 <img class="rounded-md" src="https://placeimg.com/500/500/arch" alt="Mall Image"/>
             </figure>
+=======
+        <div class="flex flex-col space-y-6 w-1/3">
+            <div class="flex flex-col space-y-4">
+                <h1 class="text-3xl font-bold">{{ $mall->name }}</h1>
+                <p class="text-lg">{{ $mall->address }}</p>
+            </div>
+            <div class="rounded-md">
+                <figure>
+                    @if ($mall->image_url == null)
+                    <img src="{{ asset('images/default.png') }}" />
+                @else
+                    <img class="rounded-xl" src="{{ asset('storage/' . $mall->image_url) }}" />
+                @endif
+                </figure>
+            </div>
+>>>>>>> Stashed changes
         </div>
         <div class="flex flex-col space-y-4 w-full">
             {{-- announcements --}}
@@ -26,6 +43,7 @@
                     </div>
                 </div>
             @endif
+<<<<<<< Updated upstream
             <div class="flex flex-col justify-between h-full">
                 <div class="flex flex-col space-y-4">
                     <h1 class="text-3xl font-bold">{{ $mall->name }}</h1>
@@ -38,6 +56,30 @@
                         <button class="btn btn-primary w-full">Reserve a Park</button>
                     </a>
                 </div>
+=======
+            <h1 class="text-xl font-bold">Available Mall Segmentations</h1>
+            <div class="flex space-x-6 overflow-x-auto">
+                @foreach ($mall->segmentations as $segmentation)
+                    <div class="card w-72 bg-base-100 shadow-lg border-2 border-base-200">
+                        <figure class="px-10 pt-10">
+                            @if ($mall->image_url == null)
+                    <img src="{{ asset('images/default.png') }}" />
+                @else
+                    <img class="rounded-xl" src="{{ asset('storage/' . $mall->image_url) }}" />
+                @endif
+                        </figure>
+                        <div class="card-body items-center text-center">
+                            <h2 class="card-title">{{ $segmentation->name }}</h2>
+                            <p>Available Parking Space : {{ $segmentation->park_space }}</p>
+                            <p>Available Reserve Space : {{ $segmentation->reserve_space }}</p>
+                            <form action="{{ route('customer.reserve', $mall->slug) }}">
+                                <input type="hidden" name="segmentation" value="{{ $segmentation->slug }}">
+                                <button class="btn btn-primary w-full" type="submit">Reserve a Park</button>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
+>>>>>>> Stashed changes
             </div>
         </div>
     </div>
